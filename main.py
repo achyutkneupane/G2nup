6c6aa8cc8e136f6df1de44467f1f9ab9615b767e
achyutkneupane <achyutkneupane@gmail.com>
2019-11-14 00:27:40 +0545

    Backup for GUI adding

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from tkinter import filedialog
 from tkinter.filedialog import askopenfilename
 import func as zoom
 
+window = Tk()
+window.title("G2NUP Reader")
+window.configure(background='black')
+window.geometry('500x300')
+window.resizable(0, 0) 
 def callback():
-    name= askopenfilename() 
+    name= askopenfilename(initialdir = "Downloads",title = "Select G2NuP file",filetypes =[('G2Nu Patch', '*.g2nup')]) 
     zoom.getdata(name)
 
-Button(text='Load File', command=callback).pack(fill=X)
+Button(text='Load File', command=callback).pack()
 mainloop()
