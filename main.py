84b2c3213a6f717ab68b1599d4bcf712db20c40b
achyutkneupane <achyutkneupane@gmail.com>
2019-11-14 10:33:07 +0545

    Working GUI outputs

--- main.py
+++ main.py
@@ -11,13 +11,6 @@ window.title("G2NUP Reader")
 window.geometry('650x300')
 window.resizable(0, 0) 
 
-tree = ttk.Treeview(window)
-tree["columns"]=("","","","")
-tree.column("#0", width=200,anchor="center")
-tree.column("#1", width=150,anchor="center")
-tree.column("#2", width=150,anchor="center")
-tree.column("#3", width=150,anchor="center")
-
 def callback():
     name= askopenfilename(initialdir = "Downloads",title = "Select G2NuP file",filetypes =[('G2Nu Patch', '*.g2nup')]) 
     getdata(name)
@@ -32,10 +25,16 @@ def getdata(file):
             n = i
     print("Patch Name: %s" % tex.replace("*"," "))
     
+    tree = ttk.Treeview(window)
+    tree["columns"]=("","","","","","","")
+    tree.column("#0", width=200,anchor="center")
+    tree.column("#1", width=150,anchor="center")
+    tree.column("#2", width=150,anchor="center")
+    tree.column("#3", width=150,anchor="center")
 
     #Compressor
     if(patch[n][0].text == "0"):
-        print("Compressor: OFF")
+        tree.insert("", 0,text="COMPRESSOR: OFF")
     else:
         num = int(patch[n][1].text)
         if(patch[n][1].text == "0"):
@@ -45,76 +44,57 @@ def getdata(file):
                 com2 = "SLOW"
         else:
             com2 = int(patch[n][3].text)
-        #tree.insert("", 0,text="Line 2", values=("2A","2b","2c"))
-        print("Compressor: %s" % ef.comp[num][0])
-        print("\t    %s: %s" %(ef.comp[num][1],patch[n][2].text))
-        print("\t    %s: %s" %(ef.comp[num][2],com2))
-        print("\t    %s: %s" %(ef.comp[num][3],(int(patch[n][4].text)+1)*2))
+        tree.insert("", 0,text="COMPRESSOR: %s" % ef.efx[num][0], values=("%s: %s" %(ef.comp[num][1],patch[n][2].text),"%s: %s" %(ef.comp[num][2],com2),"%s: %s" %(ef.comp[num][3],(int(patch[n][4].text)+1)*2)))
 
     #efx
     num = int(patch[n+1][1].text)
     if(patch[n+1][0].text == "0"):
-        print("\nEFX: OFF")
+        tree.insert("", 1,text="EFX: OFF")
     else:
-        print("\nEFX: %s" % ef.efx[num][0])
-        print("     %s: %s" %(ef.efx[num][1],int(patch[n+1][2].text)+1))
-        print("     %s: %s" %(ef.efx[num][2],patch[n+1][3].text))
-        print("     %s: %s" %(ef.efx[num][3],(int(patch[n+1][4].text)+1)*2))
+        tree.insert("", 1,text="EFX: %s" % ef.efx[num][0], values=("%s: %s" %(ef.efx[num][1],int(patch[n+1][2].text)+1),"%s: %s" %(ef.efx[num][2],patch[n+1][3].text),"%s: %s" %(ef.efx[num][3],(int(patch[n+1][4].text)+1)*2)))
 
     #znr
     num = int(patch[n+2][1].text)
     if(patch[n+2][0].text == "0"):
-        print("\nZNR: OFF")
+        tree.insert("", 2,text="ZNR: OFF")
     else:
-        print("\nZNR: %s" % ef.znr[num][0])
-        print("     %s: %s" %(ef.znr[num][1],int(patch[n+2][2].text)+1))
+        tree.insert("", 2,text="ZNR: %s" % ef.znr[num][0], values=("%s: %s" %(ef.znr[num][1],int(patch[n+2][2].text)+1),"",""))
     
     #drive
     num = int(patch[n+3][1].text)
     if(patch[n+3][0].text == "0"):
-        print("\nDRV: OFF")
+        tree.insert("", 3,text="DRV: OFF")
     else:
-        print("\nDRV: %s" % ef.drv[num][0])
-        print("     %s: %s" %(ef.drv[num][1],int(patch[n+3][2].text)))
-        print("     %s: %s" %(ef.drv[num][2],patch[n+3][3].text))
-        print("     %s: %s" %(ef.drv[num][3],(int(patch[n+3][4].text)+1)))
+        tree.insert("", 3,text="DRV: %s" % ef.drv[num][0], values=("%s: %s" %(ef.drv[num][1],int(patch[n+3][2].text)),"%s: %s" %(ef.drv[num][2],patch[n+3][3].text),"%s: %s" %(ef.drv[num][3],(int(patch[n+3][4].text)+1))))
 
     #eq
     eql = ['160','400','800','3.2K','6.4K','12K']
-    print("\nEQ: ")
+    eq = ['0','1','2','3','4','5']
     for i in range(0,6):
-        print("%sHz : %s" %(eql[i],int(patch[n+4][i+2].text)-12))
+        eq[i] = eql[i] + "Hz: " + str(int(patch[n+4][i+2].text)-12)
+    tree.insert("", 4,text="EQ LOW", values=(eq[0],eq[1],eq[2]))
+    tree.insert("", 5,text="EQ HIGH", values=(eq[3],eq[4],eq[5]))
 
     #mod
     num = int(patch[n+6][1].text)
     if(patch[n+6][0].text == "0"):
-        print("\nMOD: OFF")
+        tree.insert("", 6,text="MOD: OFF")
     else:
-        print("\nMOD: %s" % ef.mod[num][0])
-        print("     %s: %s" %(ef.mod[num][1],int(patch[n+6][2].text)*2))
-        print("     %s: %s" %(ef.mod[num][2],int(patch[n+6][3].text)+1))
-        print("     %s: %s" %(ef.mod[num][3],(int(patch[n+6][4].text)*2)))
+        tree.insert("", 6,text="MOD: %s" % ef.mod[num][0], values=("%s: %s" %(ef.mod[num][1],int(patch[n+6][2].text)*2),"%s: %s" %(ef.mod[num][2],int(patch[n+6][3].text)+1),"%s: %s" %(ef.mod[num][3],(int(patch[n+6][4].text)*2))))
 
     #Delay
     num = int(patch[n+7][1].text)
     if(patch[n+7][0].text == "0"):
-        print("\nDelay: OFF")
+        tree.insert("", 7,text="Delay: OFF")
     else:
-        print("\nDelay: %s" % ef.dly[num][0])
-        print("       %s: %s" %(ef.dly[num][1],int(patch[n+7][2].text)+1))
-        print("       %s: %s" %(ef.dly[num][2],int(patch[n+7][3].text)*2))
-        print("       %s: %s" %(ef.dly[num][3],(int(patch[n+7][4].text)*2)))
+        tree.insert("", 7,text="Delay: %s" % ef.dly[num][0], values=("%s: %s" %(ef.dly[num][1],int(patch[n+7][2].text)+1),"%s: %s" %(ef.dly[num][2],int(patch[n+7][3].text)*2),"%s: %s" %(ef.dly[num][3],(int(patch[n+7][4].text)*2))))
 
     #Reverb
     num = int(patch[n+8][1].text)
     if(patch[n+8][0].text == "0"):
-        print("\nReverb: OFF")
+        tree.insert("", 8,text="Reverb: OFF")
     else:
-        print("\nReverb: %s" % ef.rev[num][0])
-        print("       %s: %s" %(ef.rev[num][1],int(patch[n+8][2].text)+1))
-        print("       %s: %s" %(ef.rev[num][2],int(patch[n+8][3].text)))
-        print("       %s: %s" %(ef.rev[num][3],(int(patch[n+8][4].text)*2)))
-Button(text='Load File', command=callback).pack()
-
-tree.pack()
+        tree.insert("", 8,text="Reverb: %s" % ef.rev[num][0], values=("%s: %s" %(ef.rev[num][1],int(patch[n+8][2].text)+1),"%s: %s" %(ef.rev[num][2],int(patch[n+8][3].text)),"%s: %s" %(ef.rev[num][3],(int(patch[n+8][4].text)*2))))
+    tree.pack()
+Button(text='Load G2NuP', command=callback).pack()
 mainloop()
